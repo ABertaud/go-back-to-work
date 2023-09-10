15c28e14f1e1a8f282245fd5e6e06a5f12309800
BERTAUD Arthur <arthur.bertaud@polyconseil.fr>
2023-09-10 20:54:27 +0200

    mvp of extension done, snooze+popup while browsing remainning

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTab = tabs[0];
     var url = currentTab.url;
-    document.getElementById('url-display').textContent = 'URL actuelle : ' + url;
+    document.getElementById('url-display').textContent = url;
+
+    var cachedUrls = JSON.parse(localStorage.getItem('urls')) || [];
+
+    const valid = !cachedUrls.some((cachedUrl) => url.includes(cachedUrl));
+    if (valid) {
+        document.querySelector('.status p').textContent = 'Status: Valid';
+        document.querySelector('.status p').classList.remove('invalid');
+        document.querySelector('.round-status').classList.remove('round-invalid');
+        document.querySelector('.status p').classList.add('valid');
+        document.querySelector('.round-status').classList.add('round-valid');
+
+    } else {
+        document.querySelector('.status p').textContent = 'Status: Invalid';
+        document.querySelector('.status p').classList.remove('valid');
+        document.querySelector('.round-status').classList.remove('round-valid');
+        document.querySelector('.status p').classList.add('invalid');
+        document.querySelector('.round-status').classList.add('round-invalid');
+    }
 });
