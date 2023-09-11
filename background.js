9c2d52bda5121f3e9c93830ba9c6b16ee415945f
BERTAUD Arthur <arthur.bertaud@polyconseil.fr>
2023-09-11 16:38:52 +0200

    remove debugs

--- background.js
+++ background.js
@@ -2,15 +2,10 @@
 import { isValid, getCachedUrls } from './utils.js';
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    console.log(tabId, changeInfo, tab);
     if (changeInfo.status === 'complete') {
         const url = tab.url;
-
-        console.log("OVER HERE");
         var cachedUrls = await getCachedUrls();
-        console.log(cachedUrls);
         const valid = isValid(url, cachedUrls);
-        
 
         if (!valid) {
             chrome.scripting.executeScript({
