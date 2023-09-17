faf27bd3a2d3fa897ee6d329440c7f1971d02516
ABertaud <arthur.bertaud44@gmail.com>
2023-09-17 12:51:08 +0200

    fix issues where tab wasn't defined anymore and still receiving events

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
-
 import { isValid, getCachedUrls } from './utils.js';
 
 
 chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    chrome.scripting.insertCSS({
-        target: { tabId: tabId },
-        files: ['animation.css']
-    });
+    if (tab.url?.startsWith("chrome://") || tab.url?.startsWith("chrome-extension://"))
+        return undefined;
 
     if (changeInfo.status === 'complete') {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabId },
+            files: ['animation.css']
+        });
         const url = tab.url;
         var cachedUrls = await getCachedUrls();
         const valid = isValid(url, cachedUrls);
@@ -33,9 +34,31 @@ function startAnimation(tabId) {
     });
 }
 
+
+function doesTabExist(tabId) {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.get(tabId, (tab) => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else if (tab) {
+          resolve(true);
+        } else {
+          reject(chrome.runtime.lastError);
+        }
+      });
+    });
+}
+
 function repeatAnimation(tabId) {
-    startAnimation(tabId);
-    setInterval(() => {
-        repeatAnimation(tabId);
-    }, 60000); // 60000 ms = 1 minute
+    if (!tabId) {
+        return;
+    }
+    doesTabExist(tabId).then((tabExists) => {
+        startAnimation(tabId);
+        setInterval(() => {
+            repeatAnimation(tabId);
+        }, 60000); // 60000 ms = 1 minute
+    }).catch((error) => {
+        console.log("The tab has been closed.");
+    });
 }
